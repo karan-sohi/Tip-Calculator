d1f758026ec8819779fbcb1abf55ae74af361d2d
karan184 <deepkaran2490@gmail.com>
2021-08-30 14:57:01 -0700

    Reset Button Working

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 
 
 function tip_button(clicked) {
+    var ButtonsDiv = document.getElementsByClassName("user")[0]
+    var tipButtons = ButtonsDiv.getElementsByTagName("button")
+    
+    for (let i=0; i<tipButtons.length; i++) {
+        tipButtons[i].style.backgroundColor = "hsl(186, 14%, 43%)"
+        tipButtons[i].style.color = "white"
+    }
+
+    var tipButton = document.getElementById(clicked)
+    tipButton.style.color = "black"
+    tipButton.style.backgroundColor = "hsl(185, 41%, 84%)"
+}
+
+
+
+function resetFunc() {
+    var ButtonsDiv = document.getElementsByClassName("user")[0]
+    var tipButtons = ButtonsDiv.getElementsByTagName("button")
+    
+    for (let i=0; i<tipButtons.length; i++) {
+
+        if (tipButtons[i].style.color == "black") {
+            var clickedTip = tipButtons[i]
+        }
+    }
+
     var bill = parseFloat(document.getElementById("total").value)
     var totalPeople = parseInt(document.getElementById("people").value)
     var finalTip = document.getElementById("tip-value")
     var finalTotal = document.getElementById("total-value")
-    var buttonClicked = document.getElementById(clicked)
-    var tipPercent = buttonClicked.value
-    var allButtons = document.querySelectorAll("button")
+    var tipPercent = clickedTip.value
+
     
-    allButtons.forEach(button => {
-        button.style.backgroundColor = "hsl(186, 14%, 43%)"
-        button.style.color = "white"
-    })
-    buttonClicked.style.color = "black"
-    buttonClicked.style.backgroundColor = "hsl(185, 41%, 84%)"
-    
-    
-    // CALCULATIONS 
+    // CALCULATIONS
     var tipValue = (tipPercent*bill)/100
     totalBill = bill + tipValue
     tipPerPerson = tipValue / totalPeople
@@ -27,6 +44,5 @@ function tip_button(clicked) {
 
     finalTip.innerHTML = "$" + tipPerPerson
     finalTotal.innerHTML = "$" + totalPerPerson
-
 
 }
